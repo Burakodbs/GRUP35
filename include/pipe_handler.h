65b4f7131a45507b6f5ab0f9151fa468fb694574
Elif Günaydın <elifgunaydin70@gmail.com>
2024-12-25 21:33:56 +0300

    Boru (pipe) bölümü tamamlandı.

---
+++ include/pipe_handler.h
@@ -0,0 +1,6 @@
+#ifndef PIPE_HANDLER_H
+#define PIPE_HANDLER_H
+
+void execute_pipe(char **commands);
+
+#endif
---
+++ src/pipe_handler.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include "pipe_handler.h"
+#include <command_handler.h>
+#include "utils.h"
+
+
+void execute_pipe(char **commands) {
+    int num_commands = 0;
+    while (commands[num_commands] != NULL) num_commands++;
+
+    int pipes[MAX_COMMANDS][2];
+    pid_t pids[MAX_COMMANDS];
+
+    for (int i = 0; i < num_commands; i++) {
+        if (i < num_commands - 1) {
+            if (pipe(pipes[i]) < 0) {
+                perror("Pipe creation failed");
+                return;
+            }
+        }
+
+        pids[i] = fork();
+        if (pids[i] < 0) {
+            perror("Fork failed");
+            return;
+        }
+
+        if (pids[i] == 0) {
+            // Çocuk process
+
+            // Önceki komutun çıkışını bu komutun girişine bağla
+            if (i > 0) {
+                dup2(pipes[i-1][0], STDIN_FILENO);
+            }
+
+            // Bu komutun çıkışını sonraki komutun girişine bağla
+            if (i < num_commands - 1) {
+                dup2(pipes[i][1], STDOUT_FILENO);
+            }
+
+            // Kullanılmayan pipe'ları kapat
+            for (int j = 0; j < num_commands - 1; j++) {
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+            }
+
+            // Komutu çalıştır
+            char *cmd = trim(commands[i]);
+            char *args[MAX_ARGS];
+            tokenize_command(cmd, args, MAX_ARGS);
+            execvp(args[0], args);
+
+            perror("Pipe command execution failed");
+            exit(1);
+        }
+
+        // Ana process: Önceki pipe'ı kapat
+        if (i > 0) {
+            close(pipes[i-1][0]);
+            close(pipes[i-1][1]);
+        }
+    }
+
+    // Ana process: Tüm child process'leri bekle
+    for (int i = 0; i < num_commands; i++) {
+        waitpid(pids[i], NULL, 0);
+    }
+}
