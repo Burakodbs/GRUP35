722a92ce2c62bb468388e055b88df6238673336c
Burak Odabaş <burak.odabas2@ogr.sakarya.edu.tr>
2024-12-26 20:49:59 +0300

    yorum satırlari-1

--- makefile
+++ makefile
@@ -1,3 +1,9 @@
+#B221210073 - Berkay GÖÇER
+#B221210078 - Burak ODABAŞ
+#B211210021 - Buse Nur ÖĞÜNŞEN
+#B221210040 - Mert BAYIR
+#G221210073 - Elif GÜNAYDIN
+
 all: compile run
 
 compile:
--- src/command_handler.c
+++ src/command_handler.c
@@ -1,4 +1,12 @@
-// command_handler.c
+/*
+
+B221210073 - Berkay GÖÇER
+B221210078 - Burak ODABAŞ
+B211210021 - Buse Nur ÖĞÜNŞEN
+B221210040 - Mert BAYIR
+G221210073 - Elif GÜNAYDIN
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
